d5963002d5ef1d170aa09c48acb343ea7d63fefe
Rogerio Alencar Filho <geriofilho@gmail.com>
2024-02-16 15:04:50 -0300

    comments

--- comments.js
+++ comments.js
@@ -1,2 +1,13 @@
-// Create web server
+const http = require('http');
+// Create web server and listen on port 3000
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!');
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
 
